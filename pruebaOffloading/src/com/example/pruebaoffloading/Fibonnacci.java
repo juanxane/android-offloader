1d9ef8d9f7be2a6d4af4b5c30bf9780ac068793a
Juanxane <juanx.ane@gmail.com>
2015-09-30 14:56:59 -0300

    branch branches con nueva funcionalidad

--- pruebaOffloading/src/com/example/pruebaoffloading/Fibonnacci.java
+++ pruebaOffloading/src/com/example/pruebaoffloading/Fibonnacci.java
@@ -3,6 +3,7 @@ package com.example.pruebaoffloading;
 public class Fibonnacci {
 
 	//SE agrega un comentario para probar git desde consola
+	//Otro comment de un nuevo branch	
 	public int calcular(int n){
 		if (n>=0){
 			if (n==0 || n==1)
